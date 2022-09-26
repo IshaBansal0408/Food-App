fa60958e2b16099cfa4fa7e82ff679036fb72b1a
NAGARJUN BHAT <61415362+NAGARJUNBHAT@users.noreply.github.com>
2022-09-26 16:13:41 +0530

    Add files via upload

--- src/main/java/com/example/FoodApp/Models/User.java
+++ src/main/java/com/example/FoodApp/Models/User.java
@@ -3,13 +3,18 @@ package com.example.FoodApp.Models;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 
 @Entity
 public class User {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
